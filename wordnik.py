ecd35dca6dcc675a54c55159c8345fc0a714b969
Denik <den1k@me.com>
2013-09-08 03:20:42 +0200

    added wordnik api

---
+++ wordnik.py
@@ -0,0 +1,6 @@
+cd ~/wordnik_py
+
+from wordnik import *
+apiUrl = 'http://api.wordnik.com/v4'
+apiKey = '35fe51a6b87809e09500c047c3402b14396168459d65abed3'
+client = swagger.ApiClient(apiKey, apiUrl)
