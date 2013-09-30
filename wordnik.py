6b85425f6e78394cd303f84554fd3823889ea24f
kfl/tms <tms@kitefishlabs.com>
2013-09-29 22:40:36 -0400

    clearing out unused code and the audio_out dir

--- wordnik.py
+++
@@ -1,6 +0,0 @@
-cd ~/wordnik_py
-
-from wordnik import *
-apiUrl = 'http://api.wordnik.com/v4'
-apiKey = '35fe51a6b87809e09500c047c3402b14396168459d65abed3'
-client = swagger.ApiClient(apiKey, apiUrl)
